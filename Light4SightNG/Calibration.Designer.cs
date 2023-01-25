02b84c39cc24724bb55c64a4172f0c709526261a
huchzi <huchzman@web.de>
2023-01-25 15:01:07 +0100

    Added rudimentary joystick support.

--- Light4SightNG/Calibration.Designer.cs
+++ Light4SightNG/Calibration.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.Start = new System.Windows.Forms.Button();
+            this.debugBox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // Start
@@ -41,20 +45,46 @@
             this.Start.UseVisualStyleBackColor = true;
             this.Start.Click += new System.EventHandler(this.StartCalibration_Click);
             // 
+            // debugBox
+            // 
+            this.debugBox.Location = new System.Drawing.Point(55, 53);
+            this.debugBox.Multiline = true;
+            this.debugBox.Name = "debugBox";
+            this.debugBox.Size = new System.Drawing.Size(146, 105);
+            this.debugBox.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(69, 176);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
             // Calibration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 76);
+            this.ClientSize = new System.Drawing.Size(274, 301);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.debugBox);
             this.Controls.Add(this.Start);
             this.Name = "Calibration";
             this.Text = "Calibration";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox debugBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- Light4SightNG/Calibration.cs
+++ Light4SightNG/Calibration.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
+using SlimDX.DirectInput;
+using System.Collections.Generic;
 
 namespace Light4SightNG
 {
     public partial class Calibration : Form
     {
+        Guid gamepad_uid;
+        Joystick gamepad;
+        DirectInput dinput;
+        clAudioControl brightAudio;
+
         public Calibration()
         {
             InitializeComponent();
+            dinput = new DirectInput();
+            foreach (DeviceInstance di in dinput.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                debugBox.AppendText(di.ProductGuid.ToString());
+                debugBox.AppendText(";");
+                debugBox.AppendText(di.ProductName);
+                debugBox.AppendText(";\n");
+
+                if (di.ProductName == "Logitech Cordless RumblePad 2 USB") gamepad_uid = di.ProductGuid;
+            }
+
+            gamepad = new Joystick(dinput, gamepad_uid);
+            gamepad.Acquire();
+
+            debugBox.AppendText("\n----\n");
+            // debugBox.AppendText(state.GetButtons());
+
+            foreach (bool b in gamepad.GetCurrentState().GetButtons())
+            {
+                debugBox.AppendText(b.ToString());
+                debugBox.AppendText(";");
+            }
+
+
+
+            numericUpDown1.Value = 50;
+
+            brightAudio = new clAudioControl();
+            brightAudio.InitWaveContainer();
+            clSignalGeneration.CalibrationSignal(1, (double) numericUpDown1.Value / 100.0);
+
+            brightAudio.PlaySignal();
+
+            backgroundWorker1.RunWorkerAsync();
+
+
         }
 
         private void StartCalibration_Click(object sender, EventArgs e)
@@ -36,5 +79,35 @@ namespace Light4SightNG
 
         }
 
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (gamepad.GetCurrentState().GetButtons()[3]) numericUpDown1.Invoke(new MethodInvoker(incNumeric));
+                if (gamepad.GetCurrentState().GetButtons()[1]) numericUpDown1.Invoke(new MethodInvoker(decNumeric));
+                Thread.Sleep(200);
+            }
+        }
+
+        private void incNumeric()
+        {
+            if (numericUpDown1.Value <= 90) numericUpDown1.Value += 10;
+            brightAudio.StopSignal();
+            Thread.Sleep(200);
+            brightAudio.InitWaveContainer();
+            clSignalGeneration.CalibrationSignal(1, (double)numericUpDown1.Value / 100.0);
+            brightAudio.PlaySignal();
+
+        }
+
+        private void decNumeric()
+        {
+            if (numericUpDown1.Value >= 10) numericUpDown1.Value -= 10;
+            brightAudio.StopSignal();
+            Thread.Sleep(200);
+            brightAudio.InitWaveContainer();
+            clSignalGeneration.CalibrationSignal(1, (double)numericUpDown1.Value / 100.0);
+            brightAudio.PlaySignal();
+        }
     }
 }
